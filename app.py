062737def09af2d93b0eda814519dfbb8c3e9b62
zaacobrake2025 <alvaro.ye@zhglory.com.cn>
2025-11-03 13:32:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import os
 
 st.set_page_config(page_title="刹车片成本查询", layout="wide")
 
-st.title("刹车片成本查询系统")
+st.title("ZAACO成本查询系统")
 
 # ---------- 读取仓库里的 Excel ----------
 excel_file = os.path.join(os.path.dirname(__file__), "cost.xlsx")
 
 try:
     df = pd.read_excel(excel_file)
-    st.subheader("成本表内容")
+    st.subheader("2025.10.22成本表")
     st.dataframe(df)
 except FileNotFoundError:
     st.error("cost.xlsx 文件不存在，请检查仓库是否包含此文件")
@@ -21,3 +21,4 @@ except ImportError as e:
 except Exception as e:
     st.error("程序运行出错，请查看详细日志")
     st.write(str(e))
+
